f5a1aee43aa344e9ceaee10a6989eb54c8108216
Felipe <geraldofelipemcruz@gmail.com>
2022-03-13 21:26:23 -0300

    Adicionado estrutura basica para estudo do ansible, sendo uma maquina controladora, uma com o apache e php, e por fim uma maquina com o banco de dados

---
+++ ansible-host/instala-nsible.sh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#atualiza os pacotes 
+apt update -y
+apt upgrade -y
+
+# instalando o ansible 
+apt -y update
+apt -y install software-properties-common
+add-apt-repository --yes --update ppa:ansible/ansible
+apt update -y 
+apt install ansible
+apt -f install -y 
